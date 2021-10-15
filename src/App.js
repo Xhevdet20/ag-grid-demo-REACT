11d3835a7e7850970141cc8aa601099300f71d62
xhevdet kryeziu <xhevdet.kryeziu@kujtesa.com>
2021-10-15 13:43:03 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {AgGridColumn, AgGridReact} from 'ag-grid-react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
+
+const App = () => {
+
+   const columnDefs = [
+     {headerName: "Id", field: 'id'},
+     {headerName: "Name", field: 'name'},
+     {headerName: "Email", field: 'email'},
+     {headerName: "Body", field: 'body'},
+   ]
+
+   const defaultColDef = {
+     sortable: true,
+     editable: true,
+     flex:1,
+     filter: true,
+     floatingFilter: true
+   }
+
+   const onGridReady = (params) => {
+    fetch('https://jsonplaceholder.typicode.com/comments')
+      .then(resp => resp.json())
+      .then(res => params.api.applyTransaction({add: res}));
+   }
+
+   const rowSelectionType = "single";
+
+   const onSelectionChanged = (event) => {
+     console.log(event.api.getSelectedRows());
+   }
+
+   return (
+       <div className="ag-theme-alpine" style={{height: 400}}>
+           <AgGridReact
+            columnDefs={columnDefs}    
+            defaultColDef={defaultColDef}
+            onGridReady={onGridReady}
+            rowSelection={rowSelectionType}
+            onSelectionChanged={onSelectionChanged}
+               >
+               <AgGridColumn field="id"></AgGridColumn>
+               <AgGridColumn field="name"></AgGridColumn>
+               <AgGridColumn field="email"></AgGridColumn>
+               <AgGridColumn field="body"></AgGridColumn>
+           </AgGridReact>
+       </div>
+   );
+};
 
 export default App;
