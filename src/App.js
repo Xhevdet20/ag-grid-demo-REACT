75cb3e376f54b597d3718ee26029d36ccbe92e7c
xhevdet kryeziu <xhevdet.kryeziu@kujtesa.com>
2021-10-20 10:40:48 +0200

    quick filter server side with lodash

--- src/App.js
+++ src/App.js
@@ -1,37 +1,52 @@
-import React, { useState } from "react";
-import { AgGridColumn, AgGridReact } from "ag-grid-react";
+import React, { useState, useEffect } from "react";
+import { AgGridReact } from "ag-grid-react";
 import "ag-grid-enterprise";
 import "ag-grid-community/dist/styles/ag-grid.css";
 import "ag-grid-community/dist/styles/ag-theme-alpine.css";
+import _ from "lodash";
 
 const App = () => {
 	const [gridApi, setGridApi] = useState(null);
+	const [searchText, setSearchText] = useState("");
 
 	const columns = [
-		{ headerName: "Athlete", field: "athlete" },
-		{ headerName: "Age", field: "age" },
-		{ headerName: "Country", field: "country" },
-		{ headerName: "Year", field: "year" },
-		{ headerName: "Date", field: "date" },
-		{ headerName: "Sport", field: "sport" },
-		{ headerName: "Gold", field: "gold" },
-		{ headerName: "Silver", field: "silver" },
-		{ headerName: "Bronze", field: "bronze" },
-		{ headerName: "Total", field: "total" },
+		{
+			headerName: "Athlete",
+			field: "athlete",
+			filter: "agTextColumnFilter",
+			// cellRenderer: "loading",
+		},
+		{ headerName: "Age", field: "age", filter: "agTextColumnFilter" },
+		{ headerName: "Country", field: "country", filter: "agTextColumnFilter" },
+		{ headerName: "Year", field: "year", filter: "agTextColumnFilter" },
+		{ headerName: "Date", field: "date", filter: "agTextColumnFilter" },
+		{ headerName: "Sport", field: "sport", filter: "agTextColumnFilter" },
+		{ headerName: "Gold", field: "gold", filter: "agTextColumnFilter" },
+		{ headerName: "Silver", field: "silver", filter: "agTextColumnFilter" },
+		{ headerName: "Bronze", field: "bronze", filter: "agTextColumnFilter" },
+		{ headerName: "Total", field: "total", filter: "agTextColumnFilter" },
 	];
 
-	const defaultColDef = {
-		filter: true,
-		floatingFilter: true,
-		filter: "agTextColumnFilter",
-		sortable: true,
-	};
+	useEffect(() => {
+		if (gridApi) {
+			onGridReady(gridApi);
+		}
+	}, [searchText]);
+
+	const search = _.debounce((text) => {
+		setSearchText(text);
+	}, 1000);
 
 	const datasource = {
 		getRows(params) {
 			console.log(JSON.stringify(params.request, null, 1));
 			const { startRow, endRow, filterModel, sortModel } = params.request;
 			let url = "http://localhost:4000/olympic?";
+			//Quick filter
+			if (searchText) {
+				url += `q=${searchText}&`;
+			}
+
 			// sorting
 			if (sortModel.length) {
 				const { colId, sort } = sortModel[0];
@@ -57,20 +72,34 @@ const App = () => {
 		},
 	};
 
-	// register datasource with the grid
-
 	const onGridReady = (params) => {
-		setGridApi(params.api);
-
+		setGridApi(params);
+		// register datasource with the grid
 		params.api.setServerSideDatasource(datasource);
 	};
-
+	// const components = {
+	// 	loading: (params) => {
+	// 		if (params.value !== undefined) {
+	// 			return params.value;
+	// 		} else {
+	// 			return "<img src='https://www.ag-grid.com/example-assets/loading.gif'/>";
+	// 		}
+	// 	},
+	// };	defaultColDef={{ filter: true, floatingFilter: true, sortable: true }}
 	return (
 		<div>
-			<div className="ag-theme-alpine" style={{ height: "500px" }}>
+			<h1 align="center">React-App</h1>
+			<h4 align="center">Implement Infinite Scroll in ag Grid</h4>
+			<input
+				type="search"
+				placeholder="search something..."
+				style={{ padding: 10, fontSize: "105%", width: "100%", outline: 0 }}
+				onChange={(e) => search(e.target.value)}
+			/>
+			<div className="ag-theme-alpine" style={{ height: 400 }}>
 				<AgGridReact
 					columnDefs={columns}
-					defaultColDef={defaultColDef}
+					defaultColDef={{ filter: true, floatingFilter: true, sortable: true }}
 					rowModelType="serverSide"
 					onGridReady={onGridReady}
 					paginationPageSize={8}
@@ -82,5 +111,4 @@ const App = () => {
 		</div>
 	);
 };
-
 export default App;
